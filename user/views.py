5f5ef7af09188670716109ffd6de051bb7bb6bb9
aneesh-kaitou1412 <a3grand@gmail.com>
2018-10-28 05:22:32 +0530

    adds user templates and views in the app

--- user/views.py
+++ user/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import redirect
 # Create your views here.
 
 def userhome(request, username):
-	return HttpResponse("<h1> Hello User")
+	return render(request, 'userhome.html')
