0abc0db86b89cb1bd9bd79952d3847c132f137f9
aneesh-kaitou1412 <a3grand@gmail.com>
2018-10-28 05:21:43 +0530

    adds user app and template dirs

--- spc/settings.py
+++ spc/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'spc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['spc/templates', os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['spc/templates', 'user/templates' , os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
