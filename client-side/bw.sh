7c0f5395960a9de942f98d767c2f57e35dedbe52
riyakb <38978836+riyakb@users.noreply.github.com>
2018-11-22 04:20:57 +0530

    Add files via upload
    linux client

---
+++ client-side/bw.sh
@@ -0,0 +1,9 @@
+chmod +x spc.py
+mkdir -p ~/bin
+cp spc.py ~/bin/spc
+cp sync.py ~/bin/sync.py
+cp encrypt.py ~/bin/encrypt.py
+export PATH=$PATH":$HOME/bin"
+
+# sudo cp my_print /usr/local/man/man1/spc.1
+# sudo gzip /usr/local/man/man1/spc.1
