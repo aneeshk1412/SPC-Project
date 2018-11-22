ed0fc9b6a49778bb63c349082dbaed7b6769113f
aneesh-kaitou1412 <a3grand@gmail.com>
2018-11-22 19:16:47 +0530

    adds getting user data from site

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from user.models import DirFile
+from django.contrib.auth.models import User
 
 class DirFileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,8 @@ class DirFileDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = DirFile
         fields = ('pk', 'owner', 'parentId', 'name', 'md5code', 'pathLineage', 'fileContent', 'dorf', 'modifiedTime')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'username')
