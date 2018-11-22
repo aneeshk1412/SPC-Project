faf4790c14663fa45495760c0ca453171e00a685
aneesh-kaitou1412 <a3grand@gmail.com>
2018-11-23 02:19:09 +0530

    adds files for AES decryption to server

--- user/models.py
+++ user/models.py
@@ -21,6 +21,8 @@ class DirFile(models.Model):
     )
     fileContent = models.BinaryField(max_length=10000000, editable=True)
     modifiedTime = models.DateTimeField(auto_now=True)
+    encyption_scheme = models.CharField(max_length=100)
+    file_type = models.CharField(max_length=20)
 
     def __str__(self):
         uname = str(self.owner)
