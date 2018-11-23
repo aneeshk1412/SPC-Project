95faa345a5d0030730bfaf6cd5e1a4ad03496533
riyakb <riyakb@cse.iitb.ac.in>
2018-11-23 15:30:08 +0530

    adds periodic sync

--- client-side/bw.sh
+++ client-side/bw.sh
@@ -5,7 +5,19 @@ cp sync.py ~/bin/sync.py
 cp encrypt.py ~/bin/encrypt.py
 cp pythonencryptAES.py ~/bin/pythonencryptAES.py
 cp compatible_Blowfish.py ~/bin/compatible_Blowfish.py
+<<<<<<< HEAD
+cp bw_cron.py ~/bin/bw_cron.py
+
+
+
+touch crontab.cron
+echo '0 */2 * * * DISPLAY=:0 xterm -hold -e "python3 ~/bin/bw_cron.py" ' > crontab.cron
+crontab crontab.cron
+
+
+=======
 cp compatible_ARC4.py ~/bin/compatible_ARC4.py
+>>>>>>> 89f1818cf4bb93f8e9d8909fc06d51e42ed02155
 export PATH=$PATH":$HOME/bin"
 
 # sudo cp my_print /usr/local/man/man1/spc.1
---
+++ client-side/bw_cron.py
@@ -0,0 +1,7 @@
+import subprocess
+import os
+
+if os.path.exists(os.path.expanduser(os.path.join("~", "spc_details/auth.txt"))) and os.path.exists(os.path.expanduser(os.path.join("~", "spc_details/enc.txt"))) and os.path.exists(os.path.expanduser(os.path.join("~", "spc_details/dir.txt"))) and os.path.exists(os.path.expanduser(os.path.join("~", "spc_details/ur.txt"))):
+	user_response = input('Do u wish to do periodic sync (y or n): ')
+	if user_response == 'y':
+		subprocess.run('~/bin/spc sync',shell=True)
